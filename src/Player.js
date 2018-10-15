0cf753ef9de11eca25163a0eabfa6a1e1a406bda
Jason Purinton <jasonlee55@gmail.com>
2018-10-14 19:05:13 -0500

    merge player branches

--- src/Player.js
+++ src/Player.js
@@ -4,5 +4,39 @@
  * Basic player class - stores information about name, strengths, and position, but also batting (since this game was made before the DH).
  */
 export class Player {
+	/**
+	* Constructor creates a player that stores there name, pitcher or position player, whether the player is on a team yet, hitting stats, pitching stats.
+	* POSTCONDITION: inLineup, adjNum, flagged, and revealed, all now have set values.
+	*/
+	constructor(name, position) {
+		/**
+		* Player name.
+     	* @type {string}
+     	*/
+		this.playerName = name;
+		/**
+		* Position player or pitch.
+     	* @type {string}
+     	*/
+		this.position = position;
+        /**
+        * Is available to be selected or not.
+        * @type {boolean}
+        */
+        this.inLineup = false;
+		/**
+		* Hitting stats of the player.
+     	* @type {number}
+     	*/
+		this.hitStats = -1;
+		/**
+		* Revealed status of the player.
+     	* @type {number}
+     	*/
+		this.pitchStats = -1;
+	}
 
+    getName(){
+        return this.getName;
+    }
 }
