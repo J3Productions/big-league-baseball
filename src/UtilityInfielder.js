aecf5e5b9c1a700c88c3f73d0d60ef03b170a895
Jason Purinton <jasonlee55@gmail.com>
2018-10-24 18:16:00 -0500

    Started on subclasses of PositionPlayers. Also,added more sound effects to soundFx folder

---
+++ src/UtilityInfielder.js
@@ -0,0 +1,26 @@
+'use strict';
+/**
+ *Child of Infielder class
+ */
+import { Infielder } from './Infielder.js';
+/**
+ * UtilityInfielder class - stores information about name, position, batting strength, baserunning speed, whether they have been chosen on a team.
+ */
+export class UtilityInfielder extends Infielder {
+    /**
+    * Constructor creates a player that stores there name, position, hitting stats, baserunning speed and whether the player is on a team using functions inherited from the Infielder class.
+    * POSTCONDITION: name, position, hitting stats, baserunning speed all now have set values.
+        * @extends {InfielderClass}This class is a child of the Infielder class.
+        * @param {string} positionType The players position
+        */
+    constructor(name, position){
+        /**
+        * @extends {InfielderClass}
+        */
+        super(name, battStrengths, baseSpeed);
+                 /**
+        		* Position UtilityInfielder.
+             	* @type {string}
+             	*/
+        		this.position = (position);
+    }
---
+++ src/UtilityOutfielder.js
@@ -0,0 +1,26 @@
+'use strict';
+/**
+ *Child of Outfielder class
+ */
+import { Outfielder } from './Outfielder.js';
+/**
+ * UtilityOutfielder class - stores information about name, position, batting strength, baserunning speed, whether they have been chosen on a team.
+ */
+export class UtilityOutfielder extends Outfielder {
+    /**
+    * Constructor creates a player that stores there name, position, hitting stats, baserunning speed and whether the player is on a team using functions inherited from the Outfielder class.
+    * POSTCONDITION: name, position, hitting stats, baserunning speed all now have set values.
+        * @extends {OutfielderClass}This class is a child of the Outfielder class.
+        * @param {string} positionType The players position
+        */
+    constructor(name, position){
+        /**
+        * @extends {OutfielderClass}
+        */
+        super(name, battStrengths, baseSpeed);
+                 /**
+        		* Position UtilityOutfielder.
+             	* @type {string}
+             	*/
+        		this.position = (position);
+    }
