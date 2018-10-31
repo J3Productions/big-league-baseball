93e438048e0131b9979177684df196877c7b342f
Jason Purinton <jasonlee55@gmail.com>
2018-10-30 20:37:06 -0500

    Updating Team and all Player classes.

--- src/Team.js
+++ src/Team.js
@@ -3,7 +3,7 @@
  *Team class creates teams
  */
 import { Pitcher } from './Pitcher.js';
-import { PositionPlayer } from './PositionPlayer.js';
+import { Outfielder } from './Outfielder.js';
 /**
  * Team
  */
@@ -32,7 +32,7 @@ export class Team {
 	     * @type {number}
 	     */
         this.runs = 0;
-        this.createPlayers();
+        this.createPlayers(team);
     }
     /**
 	* This function returns a team of players in an array.
@@ -45,16 +45,54 @@ export class Team {
     * This function buildes players and puts them into the arrOfPlayers.
     * POSTCONDITION: No return.
     */
-    createPlayers(){
-        this.arrOfPlayers[0]= new PositionPlayer("Jielong GoLong", "catcher", ["co", "ch"], false);
-        this.arrOfPlayers[1]= new PositionPlayer("Jacob \"The Bull\" Parnell", "1base", ["fs", "fl"], true);
-        this.arrOfPlayers[2]= new PositionPlayer("Rocco Manzeene", "short", ["ci"], false);
-        this.arrOfPlayers[3]= new PositionPlayer("Larry Smerts", "2base", ["fs", "fl"], true);
-        this.arrOfPlayers[4]= new PositionPlayer("Johnny Demp", "3base", ["ss", "sl"], true);
-        this.arrOfPlayers[5]= new PositionPlayer("Ray Cobb", "left", ["fs", "fi"], false);
-        this.arrOfPlayers[6]= new PositionPlayer("Mike Truk", "center", ["si", "ci"], true);
-        this.arrOfPlayers[7]= new PositionPlayer("Mickel J-Rod", "right", ["co"], false);
-        this.arrOfPlayers[8]= new Pitcher("Boom Mannski", "pitcher", [], false, ["fs","ss","ch","fl"]);
+    createPlayers(team){
+        if(team==home){
+            this.arrOfPlayers[0]= new Catcher("Jielong GoLong", "catcher", ["co", "ch"], false);
+            this.arrOfPlayers[1]= new Infielder("Jacob \"The Bull\" Parnell", "1base", ["fs", "fl"], true);
+            this.arrOfPlayers[2]= new Infielder("Rocco Manzeene", "short", ["ci"], false);
+            this.arrOfPlayers[3]= new Infielder("Larry Smerts", "2base", ["fs", "fl"], true);
+            this.arrOfPlayers[4]= new Infielder("Johnny Demp", "3base", ["ss", "sl"], true);
+            this.arrOfPlayers[5]= new Outfielder("Ray Cobb", "left", ["fs", "fi"], false);
+            this.arrOfPlayers[6]= new Outfielder("Mike Truk", "center", ["si", "ci"], true);
+            this.arrOfPlayers[7]= new Outfielder("Mickel J-Rod", "right", ["co"], false);
+            //Bench
+            this.arrOfPlayers[8]= new Catcher("Micky Robinson", "catcher", ["si"], false);
+            this.arrOfPlayers[9]= new UtilityInfielder("john", "ui", ["fi"], true);//position "ui" == Utility Infielder
+            this.arrOfPlayers[10]= new UtilityOutfielder("Duke", "uo", ["fs"], true);//position "uo" == Utility Outfielder
+            this.arrOfPlayers[11]= new PinchHitter("pinch1", "ph", ["si","fi","ci"], false, "lefty");//position "ph" == Pinch Hitter; Bats Lefty
+            this.arrOfPlayers[12]= new PinchHitter("pinch2", "ph", ["co","ch"], true, "righty");//position "ph" == Pinch Hitter; Bats righty
+            this.arrOfPlayers[13]= new PinchHitter("pinch3", "ph", ["sh","ch"], true, "lefty&righty");//position "ph" == Pinch Hitter; Bats Lefty and Righty
+            //Pitchers
+            this.arrOfPlayers[14]= new Pitcher("Boom Mannski", "pitcher", [], false, ["ss","sl","fh","fl","fi","co","ch","ci"], righty);
+             }
+            this.arrOfPlayers[15]= new Pitcher("pitch1", "pitcher", [], false, ["ss","","","","","","","","",""], righty);
+            this.arrOfPlayers[15]= new Pitcher("pitch1", "pitcher", [], false, ["ss","","","","","","","","",""], righty);
+            this.arrOfPlayers[15]= new ReliefPitcher("pitch1", "pitcher", [], false, ["ss","","","","","","","","",""], righty);
+             }
+        else{
+            this.arrOfPlayers[0]= new Catcher("Jielong GoLong", "catcher", ["co", "ch"], false);
+            this.arrOfPlayers[1]= new Infielder("Jacob \"The Bull\" Parnell", "1base", ["fs", "fl"], true);
+            this.arrOfPlayers[2]= new Infielder("Rocco Manzeene", "short", ["ci"], false);
+            this.arrOfPlayers[3]= new Infielder("Larry Smerts", "2base", ["fs", "fl"], true);
+            this.arrOfPlayers[4]= new Infielder("Johnny Demp", "3base", ["ss", "sl"], true);
+            this.arrOfPlayers[5]= new Outfielder("Ray Cobb", "left", ["fs", "fi"], false);
+            this.arrOfPlayers[6]= new Outfielder("Mike Truk", "center", ["si", "ci"], true);
+            this.arrOfPlayers[7]= new Outfielder("Mickel J-Rod", "right", ["co"], false);
+            //Bench
+            this.arrOfPlayers[8]= new Catcher("Micky Robinson", "catcher", ["si"], false);
+            this.arrOfPlayers[9]= new UtilityInfielder("john", "ui", ["fi"], true);//position "ui" == Utility Infielder
+            this.arrOfPlayers[10]= new UtilityOutfielder("Duke", "uo", ["fs"], true);//position "uo" == Utility Outfielder
+            this.arrOfPlayers[11]= new PinchHitter("pinch1", "ph", ["si","fi","ci"], false, "lefty");//position "ph" == Pinch Hitter; Bats Lefty
+            this.arrOfPlayers[12]= new PinchHitter("pinch2", "ph", ["co","ch"], true, "righty");//position "ph" == Pinch Hitter; Bats righty
+            this.arrOfPlayers[13]= new PinchHitter("pinch3", "ph", ["sh","ch"], true, "lefty&righty");//position "ph" == Pinch Hitter; Bats Lefty and Righty
+            //Pitchers
+            this.arrOfPlayers[14]= new Pitcher("Boom Mannski", "pitcher", [], false, ["ss","sl","fh","fl","fi","co","ch","ci"], righty);
+             }
+            this.arrOfPlayers[15]= new Pitcher("pitch1", "pitcher", [], false, ["ss","","","","","","","","",""], righty);
+            this.arrOfPlayers[15]= new Pitcher("pitch1", "pitcher", [], false, ["ss","","","","","","","","",""], righty);
+            this.arrOfPlayers[15]= new ReliefPitcher("pitch1", "pitcher", [], false, ["ss","","","","","","","","",""], righty);
+             }
+        }
     }
 
 	/**
